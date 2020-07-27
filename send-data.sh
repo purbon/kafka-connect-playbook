7588634a641d7ef83e380679a947433efbb5b637
Pere Urbon <pere@confluent.io>
2020-07-27 17:43:09 +0200

    add repeat to send data multiple times

--- send-data.sh
+++ send-data.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-docker exec schema-registry /scripts/send-data.sh
+i="0"
+while true; do
+  if [[ "$i" -ge '3' ]]; then
+    break;
+  fi
+  docker exec schema-registry /scripts/send-data.sh
+  i=$[$i+1]
+  sleep 5
+done
